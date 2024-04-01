94614870357bf308618216088a269ea8f5baf9ee
Santiago Farías <49741799+santiagofh@users.noreply.github.com>
2024-04-01 16:49:12 -0300

    init
    inicio de la aventura

---
+++ EXTRAER_DATOS_TEMP.py
@@ -0,0 +1,202 @@
+#%%
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+from datetime import datetime
+from io import StringIO
+import plotly.express as px
+from io import BytesIO
+#%%
+## Temperatura Media Mensual
+url_tmm = 'https://climatologia.meteochile.gob.cl/application/mensual/temperaturaMediaMensual'
+est=[
+'340045',
+'330075',
+'330160',
+'330113',
+'330030',
+'330122',
+'330077',
+'330019',
+'330020',
+'330114',
+'330021',
+'330121',
+'330118',
+'330193',
+'320019',
+'320051',
+'320063',
+'320056',
+'320041',
+'320049'
+]
+#%%
+## Funciones
+def extraeTable(url):
+    response = requests.get(url)
+    response = requests.get(url, timeout=360)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find('table')
+        string_io_table = StringIO(str(table))
+        df = pd.read_html(string_io_table)[0]
+        return df
+    else:
+        print(f'Error al realizar la solicitud: {response.status_code}')
+        return None
+def extraer_est_fecha_de_url(url):
+    if pd.notnull(url):
+        match = re.search(r'(\d{6})/(\d{4})/(\d{1,2})$', url)
+        if match:
+            est, año, dia = match.groups()
+            return est, año, dia
+    return None, None, None  # Retorna None para cada parte si la URL no es una cadena de texto o no coincide
+def url_agregar_fecha(url,fechas):
+    ls=[]
+    for i in fechas:
+        url_year_month=url+i
+        ls.append(url_year_month)
+    return ls
+
+#%%
+# Fechas y estaciones
+inicio = datetime(2013, 1, 1)
+fin = datetime.now()
+fechas = []
+while inicio <= fin:
+    fecha_formateada = '/' + str(inicio.year) + '/' + str(inicio.month)
+    fechas.append(fecha_formateada)
+    if inicio.month == 12:
+        inicio = inicio.replace(year=inicio.year + 1, month=1)
+    else:
+        inicio = inicio.replace(month=inicio.month + 1)
+for fecha in fechas:
+    print(fecha)
+ls_est_fecha=[]
+for i in est:
+    for j in fechas:
+        ls_est_fecha.append(i+j)
+print(ls_est_fecha)
+#%%
+import time
+ls_tmm = []
+no_ls_tmm = []
+indice_ultimo_intento = 0  # Guarda el índice del último intento
+for i in ls_est_fecha[indice_ultimo_intento:]:
+    url_est_fecha = url_tmm + '/' + i
+    print(f"Intentando acceder a: {url_est_fecha}")
+    try:
+        df = extraeTable(url_est_fecha)
+        df['url'] = i
+        ls_tmm.append(df)
+        indice_ultimo_intento += 1
+    except ConnectionError as e:
+        print(f"Error de conexión: {e}")
+        no_ls_tmm.append(url_est_fecha)
+        time.sleep(5*60) 
+        continue 
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+        no_ls_tmm.append(url_est_fecha)
+        continue
+
+#%%
+        
+tmm=pd.concat(ls_tmm)
+# %%
+if isinstance(tmm.columns, pd.MultiIndex):
+    tmm.columns = ['_'.join(map(str, col)).strip() for col in tmm.columns.values]
+tmm_reduced = tmm.iloc[1:].reset_index(drop=True)
+tmm_reduced = tmm_reduced.iloc[:, [*range(11)]]
+tmm_reduced = tmm_reduced.rename(columns={tmm_reduced.columns[0]: 'day',
+                                          tmm_reduced.columns[1]: 't_min',
+                                          tmm_reduced.columns[2]: 'ht_min',
+                                          tmm_reduced.columns[3]: 't_max',
+                                          tmm_reduced.columns[4]: 'ht_max',
+                                          tmm_reduced.columns[5]: 'Climatologica',
+                                          tmm_reduced.columns[6]: 'Aritmetica',
+                                          tmm_reduced.columns[7]: 'col_7',
+                                          tmm_reduced.columns[8]: 'col_8',
+                                          tmm_reduced.columns[9]: 'col_9',
+                                          tmm_reduced.columns[10]: 'url',
+                                          })
+
+tmm_reduced['t_max'] = pd.to_numeric(tmm_reduced['t_max'], errors='coerce')
+tmm_reduced['t_min'] = pd.to_numeric(tmm_reduced['t_min'], errors='coerce')
+tmm_reduced['day'] = pd.to_numeric(tmm_reduced['day'], errors='coerce')
+tmm_reduced = tmm_reduced.dropna(subset=['t_max'])
+tmm_reduced = tmm_reduced.dropna(subset=['day'])
+#%%
+# add Est, Year Month 
+resultados = tmm_reduced['url'].apply(lambda x: extraer_est_fecha_de_url(x) if pd.notnull(x) else (None, None, None))
+temp_df = pd.DataFrame(resultados.tolist(), columns=['est', 'year', 'month'], index=tmm_reduced.index)
+#%%
+tmm_reduced_date = pd.concat([tmm_reduced, temp_df], axis=1)
+tmm_reduced_date['date'] = pd.to_datetime(tmm_reduced_date[['year', 'month', 'day']])
+tmm_faltantes = pd.DataFrame(no_ls_tmm, columns=['url_faltantes'])
+# %%
+tmm_reduced_date.to_csv("data/tmm_historico_2013_2024.csv")
+tmm_faltantes.to_csv("data/tmm_faltantes.csv") 
+# %%
+
+import plotly.express as px
+
+# Asegurándonos de que tmm_reduced_date tiene una columna 'fecha_formateada' correctamente formateada.
+if 'fecha_formateada' not in tmm_reduced_date.columns:
+    tmm_reduced_date['fecha_formateada'] = tmm_reduced_date['date'].dt.strftime('%d-%m-%Y')
+
+# Crear el gráfico con Plotly Express
+fig = px.scatter(tmm_reduced_date, x='date', y='t_max', title='Temperaturas Máximas por Día',
+                 labels={'date': 'Fecha', 't_max': 'Temperatura Máxima (°C)'},
+                 hover_data=['fecha_formateada', 'est', 't_max'])
+
+# Añadir línea horizontal en 35 grados
+fig.add_hline(y=35, line_dash="dash", line_color="red", annotation_text="35 °C", annotation_position="bottom right")
+
+# Mostrar el gráfico
+fig.show()
+
+# Contar cuántos valores están sobre la línea de 35 grados
+valores_sobre_35 = tmm_reduced_date[tmm_reduced_date['t_max'] > 35].shape[0]
+valores_sobre_35
+
+# Contar cuántos valores están sobre la línea de 35 grados
+valores_sobre_35 = tmm_reduced_date[tmm_reduced_date['t_max'] > 35].shape[0]
+valores_sobre_35
+
+# %%
+tmm_reduced_date.loc[tmm_reduced_date['t_max'] > 35]
+# %%
+estaciones_por_año = tmm_reduced_date[tmm_reduced_date['t_max'] > 35].groupby('date')['est'].nunique().reset_index()
+fig = px.bar(estaciones_por_año, x='year', y='est', title='Cantidad de Estaciones que Superaron los 35°C por Año',
+             labels={'est': 'Cantidad de Estaciones', 'year': 'Año'})
+fig.show()
+# %%
+
+# 1. Histograma de las temperaturas máximas
+fig_hist = px.histogram(tmm_reduced_date, x='t_max', nbins=50, title='Distribución de Temperaturas Máximas',
+                        labels={'t_max': 'Temperatura Máxima (°C)'})
+
+# 2. Temperaturas Máximas a lo Largo del Tiempo
+fig_scatter = px.scatter(tmm_reduced_date, x='date', y='t_max',
+                         title='Temperaturas Máximas a lo Largo del Tiempo',
+                         color='t_max', color_continuous_scale=px.colors.sequential.Inferno,
+                         labels={'date': 'Fecha', 't_max': 'Temperatura Máxima (°C)'})
+
+# 3. Cantidad de Días con Temperaturas Extremas por Año
+extremas_por_año = tmm_reduced_date[tmm_reduced_date['t_max'] > 35].groupby('year').size().reset_index(name='días_extremos')
+fig_bar_año = px.bar(extremas_por_año, x='year', y='días_extremos',
+                     title='Cantidad de Días con Temperaturas Extremas (>35°C) por Año',
+                     labels={'días_extremos': 'Días Extremos', 'year': 'Año'})
+
+# 4. Temperaturas Máximas por Estación
+extremas_por_estación = tmm_reduced_date[tmm_reduced_date['t_max'] > 35].groupby('est').size().reset_index(name='días_extremos')
+fig_bar_estación = px.bar(extremas_por_estación, x='est', y='días_extremos',
+                          title='Cantidad de Días con Temperaturas Extremas (>35°C) por Estación',
+                          labels={'días_extremos': 'Días Extremos', 'est': 'Estación'})
+
+# Displaying the first plot as an example due to execution environment constraints
+fig_scatter.show()
+# %%
---
+++ app.py
@@ -0,0 +1,86 @@
+#%%
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import pydeck as pdk
+
+# Cargar los datos (Asumiendo que ya has cargado y preparado 'df' y 'df_est' como antes)
+df = pd.read_csv("data/tmm_historico_2013_2024.csv")
+df_est = pd.read_excel("data/est_meteo.xlsx")
+
+# Funciones
+def evaluar_alertas(df):
+    df['alerta'] = 'Sin Alerta' 
+    df['mes'] = df['date'].dt.month
+    df.loc[df['mes'].isin([11, 12, 1, 2, 3]), 'alerta'] = 'Alerta temprana preventiva'
+    df.loc[df['t_max'] >= 40, 'alerta'] = 'Alerta Roja'
+    df['alerta_temporal'] = df['t_max'] >= 34
+    df['alerta_consecutiva'] = df['alerta_temporal'].rolling(window=2).sum()
+    df.loc[df['alerta_consecutiva'] >= 2, 'alerta'] = 'Alerta Amarilla'
+    df['alerta_consecutiva_3'] = df['alerta_temporal'].rolling(window=3).sum()
+    df.loc[df['alerta_consecutiva_3'] >= 3, 'alerta'] = 'Alerta Roja'
+    return df
+
+# Preparar los datos de fecha y códigos de estaciones, si aún no se ha hecho
+if not pd.api.types.is_datetime64_any_dtype(df['date']):
+    df['date'] = pd.to_datetime(df['date'])
+
+# Asegurarse de que los códigos de las estaciones estén en el formato correcto
+df['est'] = df['est'].astype(str)
+df_est['CODIGO'] = df_est['CODIGO'].astype(str)
+df_alerta=evaluar_alertas(df)
+
+st.write("# SEREMI RM - Analisis exploratorio de datos - Temperaturas extremas")
+
+# Encontrar el código de la estación basado en el nombre seleccionado
+# Crear un selectbox para seleccionar la estación
+estaciones = df_est['NOMBRE'].tolist()  # Asume que df_est tiene una columna 'NOMBRE' para los nombres de las estaciones
+indice_quinta_normal = estaciones.index('Quinta Normal') if 'Quinta Normal' in estaciones else 0
+nombre_estacion_seleccionada = st.selectbox('Selecciona una estación:', estaciones,index=indice_quinta_normal)
+codigo_estacion_seleccionada = df_est[df_est['NOMBRE'] == nombre_estacion_seleccionada]['CODIGO'].iloc[0]
+
+
+# Filtrar los datos para la estación seleccionada
+df_seleccionado = df_alerta[df_alerta.est == codigo_estacion_seleccionada]
+# Figura 1
+st.write(f"## Temperaturas máximas para la estación {nombre_estacion_seleccionada} desde el año 2013")
+fig = px.line(df_seleccionado, x='date', y='t_max', title=f'Temperaturas Máximas para {nombre_estacion_seleccionada}')
+fig.add_hline(y=34, line_dash="dot", line_color="yellow", annotation_text="34°C", annotation_position="bottom right")
+fig.add_hline(y=40, line_dash="dot", line_color="red", annotation_text="40°C", annotation_position="bottom right")
+fig.add_hline(y=30, line_dash="dot", line_color="green", annotation_text="30°C", annotation_position="bottom right")
+st.plotly_chart(fig, use_container_width=True)
+# Figura 2
+st.write(f"## Temperaturas máximas para la estación {nombre_estacion_seleccionada} desde el año 2023")
+df_seleccionado_2023 = df_seleccionado[df_seleccionado['date'] >= '2023-01-01']
+fig2 = px.line(df_seleccionado_2023, x='date', y='t_max', title=f'Temperaturas Máximas desde el año 2023 para {nombre_estacion_seleccionada}')
+fig2.add_hline(y=34, line_dash="dot", line_color="yellow", annotation_text="34°C", annotation_position="bottom right")
+fig2.add_hline(y=40, line_dash="dot", line_color="red", annotation_text="40°C", annotation_position="bottom right")
+fig2.add_hline(y=30, line_dash="dot", line_color="green", annotation_text="30°C", annotation_position="bottom right")
+
+st.plotly_chart(fig2, use_container_width=True)
+#%%
+st.write(f"## Tipos de alerta para la estación {nombre_estacion_seleccionada} desde el año 2023")
+def visualizar_alertas_con_marcadores(df):
+    # Mapeo de colores para cada tipo de alerta
+    color_map = {
+        'Sin Alerta': 'blue',  # Azul para días sin alerta
+        'Alerta temprana preventiva': 'green',        # Verde para Alerta Temprana Preventiva
+        'Alerta Amarilla': 'yellow',  # Amarillo para Alerta Amarilla
+        'Alerta Roja': 'red'   # Rojo para Alerta Roja
+    }
+    
+    # Crear la figura
+    fig = px.line(df, x='date', y='t_max', title=f'Temperaturas Máximas y Alertas para {nombre_estacion_seleccionada}', color_discrete_sequence=['grey'])
+
+    # Agregar marcadores de colores
+    for alerta, color in color_map.items():
+        df_alerta = df[df['alerta'] == alerta]
+        fig.add_scatter(x=df_alerta['date'], y=df_alerta['t_max'], mode='markers', name=alerta, marker=dict(color=color),)
+
+    return fig
+
+# Usar la función actualizada para generar y mostrar el gráfico
+fig_con_alertas_con_marcadores = visualizar_alertas_con_marcadores(df_seleccionado_2023)
+st.plotly_chart(fig_con_alertas_con_marcadores, use_container_width=True)
+
+# %%
