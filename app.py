51e914f69428c29474b67a26e0c7289471bdeb68
Santiago Farías <49741799+santiagofh@users.noreply.github.com>
2024-04-02 15:19:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,17 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
+st.write("# Bienvenido a los dashboard de temperaturas extremas para la Región Metropolitana! 👋")
+
+st.sidebar.success("Selecciona una categoria.")
+
+st.markdown(
+    """
+    Esta es una aplicacion para la visualizacion de las olas de calor y temperaturas extremas de la RM.
+    ### Desarrollo y pilotaje
+    Está desarrollado por la Unidad de Gestion de la información Estadistica.
+    
+"""
+)
 st.page_link("app.py", label="Home", icon="🏠")
 st.page_link("pages/🌡_Temperaturas_Extremas.py", label="Temperaturas Extremas",  icon="🌡")
